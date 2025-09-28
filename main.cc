367b3fe5bbcbc14b2d1843985c105b16df2c8856
Peta-T <petaemail@seznam.cz>
2025-09-28 09:04:16 +0000

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 //build: g++ -std=c++17 main.cc languageselector.cc resources.c -o app $(pkg-config --cflags --libs gtkmm-4.0)
-// build resource: glib-compile-resources --generate-source ./resource.xml
+// build resource: glib-compile-resources --generate-source ./resources.xml
 // flag images from: https://github.com/lipis/flag-icons/tree/main/flags/4x3
 #include <gtkmm.h>
 #include <iostream>
